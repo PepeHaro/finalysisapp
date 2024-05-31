517511844c62f4fffad19d3e555bec2e2a42b02d
Pepe Haro <pepeharo@Pepes-MacBook-Air-2.local>
2024-05-31 11:35:38 -0600

    Actualización

--- app.py
+++ app.py
@@ -326,13 +326,12 @@ if page == "Sector Dashboard":
         if downloaded_successfully:
             st.session_state['last_downloaded_sector'] = selected_sector
             st.success(f"The data of the {selected_index} of the {selected_sector} sector has been downloaded successfully")
-            st.write(url)
+    
     # Mostrar datos si el sector coincide, usamos st.session_state.get
     if selected_category and (selected_sector == st.session_state.get('last_downloaded_sector', '')):
         filename = f"{selected_category}.csv"
         data = pd.read_csv(filename, index_col='No.')
         st.markdown(f"### DASHBOARD: {selected_sector}:chart:")
-        st.dataframe(data)
         
         # Mostrar métricas según el ticker seleccionado
         selected_ticker = st.selectbox("Select Ticker", data['Ticker'].unique())
