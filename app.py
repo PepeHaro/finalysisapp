aea22eac0c719c84331651ed725db78f2a12f4f7
Pepe Haro <pepeharo@Pepes-MacBook-Air-2.local>
2024-05-31 12:12:29 -0600

    Actualización

--- app.py
+++ app.py
@@ -325,10 +325,11 @@ if page == "Sector Dashboard":
 
         if downloaded_successfully:
             st.session_state['last_downloaded_sector'] = selected_sector
+            st.session_state['last_downloaded_index'] = selected_index
             st.success(f"The data of the {selected_index} of the {selected_sector} sector has been downloaded successfully")
     
     # Mostrar datos si el sector e índice coinciden, usamos st.session_state.get
-    if selected_category and (selected_sector == st.session_state.get('last_downloaded_sector', '')) and (selected_index == st.session_state.get('last_downloaded_index', '')):
+    if selected_category and (selected_sector == st.session_state.get('last_downloaded_sector', 'last_downloaded_index')):
         filename = f"{selected_category}.csv"
         data = pd.read_csv(filename, index_col='No.')
         
